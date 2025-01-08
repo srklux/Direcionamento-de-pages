1aef9179aeb019d5d31028ef257628de68508c08
srklux <120310055+srklux@users.noreply.github.com>
2025-01-08 13:50:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let countdownTime = 3; // Tempo de redirecionamento em segundos
+
+// Substitua pelo seu link de afiliado
+let affiliateLink = "https://go.hotmart.com/Y97273047Y"; // Seu link de afiliado
+
+function startRedirect() {
+    setTimeout(function() {
+        window.location.href = affiliateLink; // Redireciona para o link de afiliado
+    }, countdownTime * 1000); // Redireciona após o tempo em segundos
+}
+
+// Inicia o redirecionamento
+startRedirect();
